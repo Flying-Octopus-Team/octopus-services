aa5bedaba45f6a8d5f59ef604b85812933a4d2e6
Piotr 'dyedMagenta' Frankiewicz <dyedmagenta@gmail.com>
2021-04-01 11:41:30 +0200

    #27 Move test properties to application.yml, use testcontainer jdbcUrl

--- discord-service/src/test/groovy/pl/flyingoctopus/discord/EnvironmentInitializer.groovy
+++ discord-service/src/test/groovy/pl/flyingoctopus/discord/EnvironmentInitializer.groovy
@@ -1,14 +1,11 @@
 package pl.flyingoctopus.discord
 
-
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.springframework.boot.test.util.TestPropertyValues
 import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.ConfigurableApplicationContext
 import org.testcontainers.containers.PostgreSQLContainer
 
 class EnvironmentInitializer {
+
     public static final PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres")
             .withDatabaseName("test")
             .withUsername("fobot")
@@ -19,21 +16,8 @@ class EnvironmentInitializer {
     }
 
     static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-
-        private static final Logger log = LoggerFactory.getLogger(Initializer.class);
-
         @Override
         void initialize(ConfigurableApplicationContext applicationContext) {
-            TestPropertyValues.of(PROPERTIES).applyTo(applicationContext.getEnvironment())
         }
     }
-
-    static final List<String> PROPERTIES = ["spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
-                                            "spring.flyway.url=" + postgreSQLContainer.getJdbcUrl(),
-                                            "spring.datasource.username=" + postgreSQLContainer.getUsername(),
-                                            "spring.flyway.username=" + postgreSQLContainer.getUsername(),
-                                            "spring.datasource.password=" + postgreSQLContainer.getPassword(),
-                                            "spring.flyway.password=" + postgreSQLContainer.getPassword(),
-                                            "discord.enabled=false"
-    ]
 }
